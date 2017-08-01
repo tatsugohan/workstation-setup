d420d22743593a924e1d26d20ecae1db05722c54
gohan <gohan@localhost.localdomain>
2017-08-01 11:45:38 +0200

    add vagrant to hashitools

--- ansible/files/install_hashitools.sh
+++ ansible/files/install_hashitools.sh
@@ -1,12 +1,4 @@
 set -e
-cd /tmp
-echo "**********************************************************************************"
-URL=$(curl -s https://www.vagrantup.com/downloads.html | grep x86_64.rpm | cut -d\" -f 2)
-TOOL_VERSION=$(echo $URL | cut -d_ -f2)
-echo "INSTALL VAGRANT VERSION $TOOL_VERSION"
-wget $URL -Ovagrant.rpm
-
-rpm -i vagrant.rpm
 
 echo "**********************************************************************************"
 URL=$(curl -s https://www.terraform.io/downloads.html | grep linux_amd64.zip | cut -d\" -f2)
@@ -29,3 +21,12 @@ TOOL_VERSION=$(echo $URL | cut -d_ -f2)
 echo "INSTALL VAULT VERSION $TOOL_VERSION "
 wget $URL -Ovault.zip
 unzip vault.zip -d /usr/bin
+
+cd /tmp
+echo "**********************************************************************************"
+URL=$(curl -s https://www.vagrantup.com/downloads.html | grep x86_64.rpm | cut -d\" -f 2)
+TOOL_VERSION=$(echo $URL | cut -d_ -f2)
+echo "INSTALL VAGRANT VERSION $TOOL_VERSION"
+wget $URL -Ovagrant.rpm
+
+rpm -Uh vagrant.rpm
